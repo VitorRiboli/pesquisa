72e0d17b33ea83355542e3639a18029b6d76570a
vitorriboli <vitorriboli1@gmail.com>
2023-03-02 14:03:29 -0300

    GET Records

--- backend/src/main/java/com/vitor/pesquisa/controllers/RecordController.java
+++ backend/src/main/java/com/vitor/pesquisa/controllers/RecordController.java
@@ -1,10 +1,17 @@
 package com.vitor.pesquisa.controllers;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vitor.pesquisa.dto.RecordDTO;
@@ -23,4 +30,28 @@ public class RecordController {
 		RecordDTO newDTO = service.insert(dto);
 		return ResponseEntity.ok(newDTO);
 	} 
+	
+	@GetMapping
+	public ResponseEntity<Page<RecordDTO>> findALL(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+			@RequestParam(value = "min", defaultValue = "") String min,
+			@RequestParam(value = "max", defaultValue = "") String max
+			) {
+		//Se a data vier zerada, vai receber null, se não converte a string para Instant com o parse()
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+		
+		//Se vier zerado, recebe o MAX_VALUE para mostrar tudo
+		if(linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<RecordDTO> list = service.findByMoments(minDate, maxDate, pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- backend/src/main/java/com/vitor/pesquisa/services/RecordService.java
+++ backend/src/main/java/com/vitor/pesquisa/services/RecordService.java
@@ -3,10 +3,10 @@ package com.vitor.pesquisa.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-
 
 import com.vitor.pesquisa.dto.RecordDTO;
 import com.vitor.pesquisa.dto.RecordInsertDTO;
@@ -39,5 +39,15 @@ public class RecordService {
 		
 		repository.save(entity);
 		return new RecordDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}	
+	
+	
+	
+	
+	
 }
