efd6f99b2e9627124e57c65427d7dffe3008607f
vitorriboli <vitorriboli1@gmail.com>
2023-03-06 12:32:41 -0300

    Created Header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+import { ReactComponent as Logo } from "../../assets/imgs/logo.svg";
+//import logo  from "../../assets/imgs/logo.svg"; O jeito acima trata a imagem como um component
+
+export default function Header() {
+  return (
+    <header className="main-header">
+      <Logo />
+      <div className="logo-text">
+        <span className="logo-text-1">Survey </span>
+        <span className="logo-text-2">Gaming</span>
+      </div>
+    </header>
+  )
+}
