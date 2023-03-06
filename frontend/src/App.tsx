ec3d93c81faf340cb31bad622e628dce4a92461c
vitorriboli <vitorriboli1@gmail.com>
2023-03-06 13:28:06 -0300

    Routes, React-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css';
+import "./App.css";
 
-import Header from './components/Header';
-import HomePage from './pages/ClientPage/HomePage';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import HomePage from "./pages/ClientPage/HomePage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <Header />
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import "./styles.css";
+import { Outlet } from "react-router-dom";
+
 import { ReactComponent as Logo } from "../../assets/imgs/logo.svg";
 //import logo  from "../../assets/imgs/logo.svg"; O jeito acima trata a imagem como um component
 
@@ -10,6 +12,8 @@ export default function Header() {
         <span className="logo-text-1">Survey </span>
         <span className="logo-text-2">Gaming</span>
       </div>
+
+      <Outlet />
     </header>
   )
 }
