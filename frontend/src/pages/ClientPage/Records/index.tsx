2922485756a424ab81e2b62a25218c165729d9cf
vitorriboli <vitorriboli1@gmail.com>
2023-03-08 19:28:54 -0300

    Layout Page records

--- frontend/src/pages/ClientPage/Records/index.tsx
+++ frontend/src/pages/ClientPage/Records/index.tsx
@@ -1,11 +1,52 @@
-import { Outlet } from "react-router-dom";
 import "./styles.css";
+
+import { Outlet } from "react-router-dom";
 
 export default function Records() {
   return (
     <>
-      <main>
-        teste
+      <main className="page-container">
+        <table className="records-table" cellPadding={"0"} cellSpacing={"0"} >
+          <thead>
+            <tr>
+              <th>INSTANTE</th>
+              <th>Nome</th>
+              <th>Idade</th>
+              <th>Plataforma</th>
+              <th>Gênero</th>
+              <th>Título do game</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>20/08/2020 13:45</td>
+              <td>João da Silva</td>
+              <td>23</td>
+              <td>Playstation</td>
+              <td>Ação - Aventura</td>
+              <td>The Last of Us 2</td>
+            </tr>
+
+            <tr>
+              <td>20/08/2020 13:45</td>
+              <td>João da Silva</td>
+              <td>23</td>
+              <td>Playstation</td>
+              <td>Ação - Aventura</td>
+              <td>The Last of Us 2</td>
+            </tr>
+
+            <tr>
+              <td>20/08/2020 13:45</td>
+              <td>João da Silva</td>
+              <td>23</td>
+              <td>Playstation</td>
+              <td>Ação - Aventura</td>
+              <td>The Last of Us 2</td>
+            </tr>
+
+          </tbody>
+        </table>
       </main>
 
       <Outlet />
