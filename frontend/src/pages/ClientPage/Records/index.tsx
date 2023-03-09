3984aa9244dd38ef6428e5c1dcea90127fce961d
vitorriboli <vitorriboli1@gmail.com>
2023-03-09 07:50:03 -0300

    records: dynamic rendering

--- frontend/src/pages/ClientPage/Records/index.tsx
+++ frontend/src/pages/ClientPage/Records/index.tsx
@@ -4,29 +4,25 @@ import axios from "axios";
 import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { RecordsResonse } from "./types";
-
+import { formatDate } from "./utils";
 
 const BASE_URL = "http://localhost:8080";
 
 export default function Records() {
- 
   const [records, setRecords] = useState<RecordsResonse>();
 
   console.log(records);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/records?linesPerPage=12`)
-      .then(res => {
-        setRecords(res.data);
-      })
-
-
+    axios.get(`${BASE_URL}/records?linesPerPage=12`).then((res) => {
+      setRecords(res.data);
+    });
   }, []);
 
   return (
     <>
       <main className="page-container">
-        <table className="records-table" cellPadding={"0"} cellSpacing={"0"} >
+        <table className="records-table" cellPadding={"0"} cellSpacing={"0"}>
           <thead>
             <tr>
               <th>INSTANTE</th>
@@ -38,33 +34,16 @@ export default function Records() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>20/08/2020 13:45</td>
-              <td>João da Silva</td>
-              <td>23</td>
-              <td>Playstation</td>
-              <td>Ação - Aventura</td>
-              <td>The Last of Us 2</td>
-            </tr>
-
-            <tr>
-              <td>20/08/2020 13:45</td>
-              <td>João da Silva</td>
-              <td>23</td>
-              <td>Playstation</td>
-              <td>Ação - Aventura</td>
-              <td>The Last of Us 2</td>
-            </tr>
-
-            <tr>
-              <td>20/08/2020 13:45</td>
-              <td>João da Silva</td>
-              <td>23</td>
-              <td>Playstation</td>
-              <td>Ação - Aventura</td>
-              <td>The Last of Us 2</td>
-            </tr>
-
+            {records?.content.map((record) => (
+              <tr key={record.id}>
+                <td>{formatDate(record.moment)}</td>
+                <td>{record.name}</td>
+                <td>{record.age}</td>
+                <td className="text-secondary" >{record.platform}</td>
+                <td>{record.genreName}</td>
+                <td className="text-primary">{record.gameTitle}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </main>
