8bcdf2f0271ac43abb5101d634b2a5327db7f376
vitorriboli <vitorriboli1@gmail.com>
2023-03-12 10:18:15 -0300

    Dynamic request apexcharts

--- frontend/src/pages/ClientPage/Charts/index.tsx
+++ frontend/src/pages/ClientPage/Charts/index.tsx
@@ -1,24 +1,53 @@
 import "./styles.css";
 
+import React, { useState, useEffect } from "react";
 import Filters from "../../../components/Filters";
-
-import * as chartOptions from "./chart-options";
-
+import { barOptions, pieOptions } from "./chart-options";
 import Chart from "react-apexcharts";
+import axios from "axios";
+import { BASE_URL } from "../../../utils";
+import { buildBarSeries, getGenderChartData, getPlatformChartData } from "./utils";
 
 
-const chartData = [
-  {
-    x: "vitor",
-    y: 10
-  },
-  {
-    x: "nadila",
-    y: 15
-  },
-]
-  
+type PieChartData = {
+  labels: string[];
+  series: number[];
+}
+
+type BarChartData = {
+  x: string;
+  y: number;
+}
+
+const initialPieData = {
+  labels: [],
+  series: []
+}
+
 export default function Charts() {
+
+  const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
+  const [platformData, setPlatformData] = useState<PieChartData>(initialPieData);
+  const [genderData, setGenderData] = useState<PieChartData>(initialPieData);
+
+  useEffect(() => {
+    async function getData() {
+      const recordsResponse = await axios.get(`${BASE_URL}/records`);
+      const gamesResponse = await axios.get(`${BASE_URL}/games`);
+
+      const barData = buildBarSeries(gamesResponse.data, recordsResponse.data.content);
+      setBarChartData(barData);
+
+      const platformChartData = getPlatformChartData(recordsResponse.data.content);
+      setPlatformData(platformChartData);
+
+      const genderChartData = getGenderChartData(recordsResponse.data.content);
+      setGenderData(genderChartData);
+    } 
+
+    getData();
+  }, []);
+
   return (
     <div className="page-container">
       <Filters link={"/records"} linkText={"VER TABELA"} />
@@ -27,11 +56,33 @@ export default function Charts() {
           <h1 className="top-related-title">Jogos mais votados</h1>
           <div className="games-container">
             <Chart
-              options={chartOptions.barOptions}
+              options={barOptions}
               type="bar"
               width={"900"}
-              height={"120"}
-              series={[{ data: chartData}]}
+              height={"650"}
+              series={[{ data: barChartData }]}
+            />
+          </div>
+        </div>
+
+        <div className="charts">
+          <div className="platform-chart">
+            <h2 className="chart-title">Plataformas</h2>
+            <Chart
+              options={{ ...pieOptions, labels: platformData?.labels }}
+              type="donut"
+              series={platformData?.series}
+              width="350"
+            />
+          </div>
+
+          <div className="gender-chart">
+            <h2 className="chart-title">Gêneros</h2>
+            <Chart
+              options={{ ...pieOptions, labels: genderData?.labels }}
+              type="donut"
+              series={genderData?.series}
+              width="350"
             />
           </div>
         </div>
